59d1c92c016b77f14e553034b257e654ddd2fdd8
evercodex <evercodex@gmail.com>
2018-02-20 12:12:06 +0300

    new rinkeby api url for etherscan node

--- app/scripts/nodeHelpers/etherscanRin.js
+++ app/scripts/nodeHelpers/etherscanRin.js
@@ -4,5 +4,5 @@ var rinkeby = {};
 for (var attr in _ethscan) {
     rinkeby[attr] = _ethscan[attr];
 }
-rinkeby.SERVERURL = 'https://rinkeby.etherscan.io/api';
+rinkeby.SERVERURL = 'https://api-rinkeby.etherscan.io/api';
 module.exports = rinkeby;
